b35150409bf428160226f8ae493eda6c736416ad
Carl Pettersson <carl.pettersson@valentin.se>
2011-12-08 15:05:23 +0100

    Initial commit

---
+++ jquery.prettyCheckbox.js
@@ -0,0 +1,66 @@
+(function ($) {          
+    var pluginName = 'prettyCheckbox',
+        defaults = {
+            wrapperClass: 'pretty-checkbox',
+            checkboxClass: 'checkbox',
+            setErrorClass: function (nativeCheckbox, prettyCheckbox) {}
+        };
+ 
+    function PrettyCheckbox(element, options) {
+        this.element = element;
+        this.options = $.extend({}, defaults, options) ;
+        this._defaults = defaults;
+        this._name = pluginName;
+
+        this.init();
+    }
+ 
+    PrettyCheckbox.prototype.init = function () {
+        var $checkboxes = $(this.element).find(':checkbox'),
+            options = this.options;
+        
+        $checkboxes.each(function () {
+            var $nativeCheckbox = $(this),
+                $label = $nativeCheckbox.closest('label'),
+                labelContent = $label.html(),
+                $prettyCheckbox = $('<div class="' + options.wrapperClass + '"><div class="' + options.checkboxClass + '"></div>' + labelContent + '</div>');
+                $prettyCheckbox.find('input').remove();
+
+            // Determine if checkbox is checked by defualt
+            if ($nativeCheckbox.is(':checked')) {
+                $prettyCheckbox.addClass('checked');
+            }
+                
+            // Toggle checked/unchecked on click    
+            $prettyCheckbox.click(function (e) {
+                $(this).toggleClass('checked');
+                if ($nativeCheckbox.attr('checked')) {
+                    $nativeCheckbox.removeAttr('checked');
+                } else {
+                    $nativeCheckbox.attr('checked', 'checked');               
+                    if ($prettyCheckbox.hasClass('error')) {
+                        $prettyCheckbox.removeClass('error');
+                    }
+                }
+                
+                e.preventDefault();
+            }) // Prevent parent event from firing if labeltext has anchor inside
+            .find('a').click(function (e) {
+                e.stopPropagation();
+            });
+            
+            options.setErrorClass($nativeCheckbox[0], $prettyCheckbox[0]);
+            
+            $label.hide().after($prettyCheckbox);
+        });
+    };
+
+    $.fn[pluginName] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName,
+                new PrettyCheckbox(this, options));
+            }
+        });
+    }
+})(jQuery);
